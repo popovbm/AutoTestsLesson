a653e758fef9a3dc248cecfc6b8cd181ade4c98a
Ivan Popov <popovbm@gmail.com>
2022-07-20 14:22:51 +0300

    end 1st theory

--- src/StringUtils.php
+++ src/StringUtils.php
@@ -7,7 +7,7 @@ function capitalize($text)
     if ($text === '') {
         return '';
     }
-    $firstSymbol = mb_strtoupper($text[0]);
-    $restSubstring = mb_substr($text, 1);
+    $firstSymbol = strtoupper($text[0]);
+    $restSubstring = substr($text, 1);
     return "{$firstSymbol}{$restSubstring}";
 }
--- tests/StringUtilsTest.php
+++ tests/StringUtilsTest.php
@@ -3,10 +3,11 @@
 require("src/StringUtils.php");
 
 if (StringUtils\capitalize('hello') !== 'Hello') {
-    throw new \Exception('Функция работает неверно');
+    throw new \Exception('Функция работает неверно!');
 }
+
 if (StringUtils\capitalize('') !== '') {
-    throw new \Exception('Функция работает неверно');
+    throw new \Exception('Функция работает неверно!');
 }
 
 echo 'Все тесты пройдены!';
