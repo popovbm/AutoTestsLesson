91e74c3c944a60af167fd8c37cb3486e72d6028f
Ivan Popov <popovbm@gmail.com>
2022-07-20 14:07:56 +0300

    add code

---
+++ src/StringUtils.php
@@ -0,0 +1,10 @@
+<?php
+function capitalize($text)
+{
+    if ($text === '') {
+        return '';
+    }
+    $firstSymbol = mb_strtoupper($text[0]);
+    $restSubstring = mb_substr($text, 1);
+    return "{$firstSymbol}{$restSubstring}";
+}
